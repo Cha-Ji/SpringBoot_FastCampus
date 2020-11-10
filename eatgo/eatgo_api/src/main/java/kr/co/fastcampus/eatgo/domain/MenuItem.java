c2a588acb91cb4b1458961ef44cb4801af56243b
차지원 <chajiwon100785@gmail.com>
2020-11-10 11:23:17 +0900

    25강 메뉴관리 : 메뉴 수정, 삭제

--- eatgo/eatgo_api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
+++ eatgo/eatgo_api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity //MenuItem에 대한 repository를 구현하지 않고도 사용할 수 있다.
 @Getter
@@ -22,4 +24,7 @@ public class MenuItem {
 
     private String name;
 
+    @Transient
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private boolean destroy;
 }
