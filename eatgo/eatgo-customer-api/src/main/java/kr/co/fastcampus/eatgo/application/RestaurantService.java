c762b162e7c97658a23bacce36d59b50a5b39755
차지원 <chajiwon100785@gmail.com>
2020-11-24 02:56:36 +0900

    30강 프로젝트 분리 : 관리자, 소비자, common 으로 나누었는데 import 설정이 뒤죽박죽이다.

---
+++ eatgo/eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/application/RestaurantService.java
@@ -0,0 +1,54 @@
+package kr.co.fastcampus.eatgo.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+    private RestaurantsRepository restaurantRepository;
+    private MenuItemRepository menuItemRepository;
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public RestaurantService(RestaurantsRepository restaurantRepository,
+                             MenuItemRepository menuItemRepository,
+                             ReviewRepository reviewRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.menuItemRepository = menuItemRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        List<Restaurant> restaurants = restaurantRepository.findAll();
+        return restaurants;
+    }
+
+    public Restaurant getRestaurant(Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> new RestaurantNotFoundException(id));
+
+        List<MenuItem> menuItems = menuItemRepository.findAllByRestaurantId(id);
+        restaurant.setMenuItems(menuItems);
+
+        List<Review> reviews = reviewRepository.findAllByRestaurantId(id);
+        restaurant.setReviews(reviews);
+
+        return restaurant;
+    }
+
+    public Restaurant addRestaurant(Restaurant restaurant) {
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Transactional
+    public Restaurant updateRestaurant(long id, String name, String address) {
+        Restaurant restaurant = restaurantRepository.findById(id).orElse(null);
+
+        restaurant.updateInformation(name, address);
+        return restaurant;
+    }
+}
